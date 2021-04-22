ce639b1132aab3322f38e71fe4986814f55540bf
Zac Todd <zactodd0@gmail.com>
2021-04-23 00:24:36 +1200

    Update leaderboard.jl

--- scrapng/leaderboard.jl
+++ scrapng/leaderboard.jl
@@ -1,7 +1,7 @@
 using HTTP, Gumbo, Cascadia, Dates, JSON, KissThreading
 
 PROFILE_URL = "https://colonist.io/api/profile" #"https://colonist.io/profile"
-COOKIE = "_ga=GA1.2.456225344.1599180098; Indicative_a38719f2-d919-446b-b2e3-0da55a22a29a=\"%7B%22defaultUniqueID%22%3A%22dda3b884-9719-44c7-8a3f-0e58b3cee020%22%7D\"; __qca=P0-58763845-1599180099331; __stripe_mid=db19d11e-0bcf-4b6f-9159-ecd3accd3ccb282899; _gid=GA1.2.1609056974.1604216954; __cfduid=dde81de7409c5333a4aa84bd790ac87ae1604481036; _gat_gtag_UA_111427971_1=1; jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyNTIxNTE4IiwiaWF0IjoxNjA0NjMzODY0LCJleHAiOjE2MDcyMjU4NjQsImF1ZCI6Imh0dHBzOi8vY29sb25pc3QuaW8vIiwiaXNzIjoiaHR0cHM6Ly9jb2xvbmlzdC5pby8ifQ.NfOGuPDgo52zWBQ6JJ9koj2oLkn1n3EfJE747_14Kqk"
+COOKIE = "ga=GA1.2.456225344.1599180098; Indicative_a38719f2-d919-446b-b2e3-0da55a22a29a=\"%7B%22defaultUniqueID%22%3A%22dda3b884-9719-44c7-8a3f-0e58b3cee020%22%7D\"; __qca=P0-58763845-1599180099331; __stripe_mid=db19d11e-0bcf-4b6f-9159-ecd3accd3ccb282899; __cfduid=d059248e9acf639da6354e2915de014461617867544; _gid=GA1.2.1696456463.1618990930; jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyNTIxNTE4IiwiaWF0IjoxNjE5MDkyODc1LCJleHAiOjE2MjE2ODQ4NzUsImF1ZCI6ImNvbG9uaXN0LmlvIiwiaXNzIjoiY29sb25pc3QuaW8ifQ.i0BoKkIpd1sFRSfg-5xBFBUakDK2Cdoj2DOknKgpKqA"
 
 
 user_url(user) = "$PROFILE_URL/$user"
@@ -28,6 +28,7 @@ function get_players(start_user="ZacTodd")
                 push!(seen, np)
                 push!(q, np)
             end
+
         end
 
         l = length(players_winrate)
@@ -77,4 +78,5 @@ function eval_player_info(player_name, seen_players)
 end
 
 
+println("Startng")
 get_players()
